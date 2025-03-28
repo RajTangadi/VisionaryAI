f2b3ad3fd26e105f659f3f0b39a58b4853763e62
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-28 13:57:59 +0530

    connect mongodb to the server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { connectDB } from "./db/connect.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-const PORT = 4000;
+const PORT = 4001;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });

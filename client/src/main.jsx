c39aa7c937f4547e227eafffb8e161f7ad6d05de
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-28 17:21:51 +0530

    shadcn setup with added clickspark button

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ClickSpark from './components/ClickSpark.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ClickSpark>
     <App />
+    </ClickSpark>
   </StrictMode>,
 )
